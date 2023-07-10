2bf4a751e64165e20624e206f0cb3ec7ef23ef71
Nick1796 <132022797+Nick1796@users.noreply.github.com>
2023-07-09 20:44:36 -0400

    Add files via upload
    new HW

---
+++ HW5/script.js
@@ -0,0 +1,49 @@
+//const a = prompt('enter number')
+//console.log(a/100*10)
+
+
+//const number = prompt('enter number ')
+//const number2 = prompt('enter number2')
+//if (number2 < number){
+   // console.log('small this: '+ number);
+//}
+//else{
+  //   console.log( 'small this: '+ number2);
+//}
+
+// const myNumber = prompt('enter your number')
+
+// if(myNumber > 100) {
+  //   console.log('its more than 100')
+ //} else if(myNumber == 100) {
+  //   console.log('it is equal')
+ //} else {
+  //   console.log('its less than 100')
+// }
+
+//const name = prompt('Enter your name ')
+//const age = prompt('enter your age')
+//if (age <= 18){
+  ///  console.log('Hi '+ name);
+//}
+//else{
+   //  console.log( 'Hello: '+ name);
+//}
+
+
+ const myName = 'Nick'
+ const myAge = 26
+ const myDog = 'Berta' 
+ const myLastName = 'Zorin'
+ const myTown = 'NewYork'
+
+ console.log(`Hello, my name is ${myName}, my age is ${myAge}, myDog is ${myDog}, my LastName  ${myLastName}, I live in ${myTown} `)
+
+
+
+
+ 
+
+
+
+
