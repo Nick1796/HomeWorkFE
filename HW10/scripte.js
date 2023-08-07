db87bff59a2766e5f424765bf142b82d9ed41719
Nick1796 <132022797+Nick1796@users.noreply.github.com>
2023-08-06 20:23:55 -0400

    Add files via upload
    new HW

---
+++ HW10/scripte.js
@@ -0,0 +1,34 @@
+// const createImage = document.querySelector('#createImage')
+// const imageContainer = document.querySelector('#imageContainer')
+
+// createImage.addEventListener("click", function() {
+
+//     const image = document.createElement("img");
+//     image.src = '../img/Starta.jpg'
+
+//     imageContainer.append(image);
+// });
+
+//!!!!!!!!!!!!!почему то не выходит картинка в нормальном качестве !!!!!!!!!!!!!
+
+ 
+//     const createParagraphBtn = document.getElementById("createParagraph");
+//     const paragraphContainer = document.getElementById("paragraphContainer");
+
+//     createParagraph.addEventListener("click", function () {
+//         const paragraphText = generateRandomText(); 
+//         const paragraphElement = document.createElement("p");
+//         paragraphElement.textContent = paragraphText;
+
+//         paragraphContainer.appendChild(paragraphElement);
+//     });
+
+//     function generateRandomText() {
+//         const texts = [
+//             "Привет."
+//         ];
+
+//         const randomIndex = Math.floor(Math.random() * texts.length);
+//         return texts[randomIndex];
+//     }
+// ;
